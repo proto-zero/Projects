3b8eb156704dc9df17edd9e4c2d838d8377608d6
Sean Lenhart <seanlenhart@Teletraan-One.local>
2020-12-30 17:22:33 -0500

    updated code to class/definition structure, introduced clear screens, styled the introduction section.

--- wopr.rb
+++ wopr.rb
@@ -1,86 +1,131 @@
-# Logon screen
-puts 'LOGON:'
-puts
-login = gets.chomp.upcase
+class WOPR
 
-# Logon requires a password
-while login != 'JOSHUA'
-  if login == 'HELP LOGON'
-    puts
-    puts 'HELP NOT AVAILABLE'
-    puts
+# Logon Screen
+  def initialize
+    system("clear")
     puts 'LOGON:'
     puts
-  elsif login != 'JOSHUA'
-    puts
-    puts 'IDENTIFICATION NOT RECOGNIZED BY SYSTEM'
-    puts '--CONNECTION TERMINATED--'
-    puts
-    return
+    login = gets.chomp.upcase
+    password login
   end
-  login = gets.chomp.upcase
-end
 
-# Introduction
-puts
-puts 'GREETINGS PROFESSOR FALKEN.'
-puts
+# Logon requires a password
+  def password x
+    while x != 'JOSHUA'
+      if x == 'HELP LOGON'
+        puts
+        puts 'HELP NOT AVAILABLE'
+        puts
+        puts 'LOGON:'
+        puts
+      elsif x != 'JOSHUA'
+        puts
+        puts 'IDENTIFICATION NOT RECOGNIZED BY SYSTEM'
+        puts '--CONNECTION TERMINATED--'
+        puts
+        exit
+      end
+      x = gets.chomp.upcase
+    end
+    introduction
+  end
 
-gets
+# Introduction to WOPR
+  def introduction
+    system("clear")
+    sleep 1
+    5.times do
+      puts '...'
+    end
+    sleep 1
+    system("clear")
+    puts
+    sleep 1
+    puts 'GREETINGS PROFESSOR FALKEN.'
+    puts
 
-puts
-puts 'HOW ARE YOU FEELING TODAY?'
-puts
+    gets.chomp
 
-gets
+    sleep 1
+    puts
+    puts
+    puts 'HOW ARE YOU FEELING TODAY?'
+    puts
 
-puts
-puts 'EXCELLENT. IT\'S BEEN A LONG TIME. CAN YOU EXPLAIN THE REMOVAL OF'
-puts 'YOUR USER ACCOUNT ON JUNE 23 1973?'
-puts
+    gets.chomp
+
+    sleep 1
+    puts
+    puts
+    puts 'EXCELLENT. IT\'S BEEN A LONG TIME. CAN YOU EXPLAIN THE REMOVAL OF'
+    puts 'YOUR USER ACCOUNT ON JUNE 23 1973?'
+    puts
+    response = gets.chomp.upcase
+    mistake response
+  end
 
 # Another passphrase
-mistakes = gets.chomp.upcase
-if !mistakes.include?('MISTAKE')
-  puts
-  puts 'IDENTIFICATION NOT RECOGNIZED BY SYSTEM'
-  puts '--CONNECTION TERMINATED--'
-  puts
-  return
-end
+  def mistake x
+    if !x.include?('MISTAKE')
+      sleep 1
+      puts
+      puts
+      puts 'IDENTIFICATION NOT RECOGNIZED BY SYSTEM'
+      puts '--CONNECTION TERMINATED--'
+      puts
+      exit
+    end
+    invitation
+  end
 
 # Game invitation
-puts
-puts 'YES THEY DO.'
-puts
-puts 'SHALL WE PLAY A GAME?'
-puts
+  def invitation
+    sleep 1
+    puts
+    puts
+    puts 'YES THEY DO.'
+    sleep 2
+    puts
+    puts 'SHALL WE PLAY A GAME?'
+    puts
 
 # Global Thermonuclear War part 01
-game = gets.chomp.upcase
-if !game.include?('GLOBAL THERMONUCLEAR WAR')
-  puts
-  puts 'IDENTIFICATION NOT RECOGNIZED BY SYSTEM'
-  puts '--CONNECTION TERMINATED--'
-  puts
-  return
-end
+    game = gets.chomp.upcase
+    if !game.include?('GLOBAL THERMONUCLEAR WAR')
+      sleep 1
+      puts
+      puts
+      puts 'IDENTIFICATION NOT RECOGNIZED BY SYSTEM'
+      puts '--CONNECTION TERMINATED--'
+      puts
+      exit
+    end
 
-puts
-puts 'WOULDN\'T YOU PREFER A GOOD GAME OF CHESS?'
-puts
+    sleep 1
+    puts
+    puts
+    puts 'WOULDN\'T YOU PREFER A GOOD GAME OF CHESS?'
+    puts
 
 # Global Thermonuclear War part 02
-answer = gets.chomp.upcase
-if !answer.include?('GLOBAL THERMONUCLEAR WAR')
-  puts
-  puts 'IDENTIFICATION NOT RECOGNIZED BY SYSTEM'
-  puts '--CONNECTION TERMINATED--'
-  puts
-  return
-end
+    answer = gets.chomp.upcase
+    if !answer.include?('GLOBAL THERMONUCLEAR WAR')
+      sleep 1
+      puts
+      puts
+      puts 'IDENTIFICATION NOT RECOGNIZED BY SYSTEM'
+      puts '--CONNECTION TERMINATED--'
+      puts
+      exit
+    end
 
 # End
-puts
-puts 'FINE.'
-puts
+    sleep 1
+    puts
+    puts
+    puts 'FINE.'
+    puts
+  end
+end
+
+WOPR.new
